16c2e977418e269725c88f2abae2e50c6bb93d67
duan spot <brandennjonge@gmail.com>
2023-08-27 00:59:14 +0300

    assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,breed):
+        self.breed=breed
+        self._name=name
+
+
+    def get_name(self,breed):
+        return self._name and self.breed
+
+    def set_name(self,name,breed):
+        if(type(name)in (str)) and  (1 >= name <=25 ):
+            pass
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+
+        self._name=name
+
+
+
+    name=property(set_name(),get_name(),)
+    breed=property(set_name(),get_name())
